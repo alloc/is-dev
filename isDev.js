a159af33b744527d71418c4f1295ff7213ae5677
aleclarson <alec.stanford.larson@gmail.com>
2016-11-16 11:38:48 -0800

    2.0.0

--- isDev.js
+++ isDev.js
@@ -1,3 +1,6 @@
-require('isNodeJS');
-var exposeToGlobal = require('exposeToGlobal');
-module.exports = exposeToGlobal("isDev", isNodeJS ? process.env.__DEV__ === "true" : global.__DEV__ === true);
+
+var isNodeJS = require('isNodeJS');
+
+module.exports = isNodeJS ?
+  process.env.__DEV__ === "true" :
+  global.__DEV__ === true;
